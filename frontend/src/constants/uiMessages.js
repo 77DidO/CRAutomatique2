824d1f7fbee8c4a94a89ba0fbe499dcb2f1b5569
DidO <didolapoub@gmail.com>
2025-09-19 12:18:30 +0200

    feat: rendre statique le sous-titre du tableau de bord

---
+++ frontend/src/constants/uiMessages.js
@@ -0,0 +1,5 @@
+export const DEFAULT_DASHBOARD_HERO_SUBTITLE = "Automatisez la rédaction de vos comptes rendus audio en toute sérénité : nous transcrivons, structurons et archivons vos dossiers en quelques minutes.";
+
+export default {
+  DEFAULT_DASHBOARD_HERO_SUBTITLE
+};
